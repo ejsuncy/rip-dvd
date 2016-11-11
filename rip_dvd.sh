12d36799cf6a73200fba69990ae3f5a035615f47
Dan Bunker <dbunked@gmail.com>
2016-11-10 21:28:07 -0700

    Update rip_dvd.sh
    add full path for /usr/bin/cut

--- rip_dvd.sh
+++ rip_dvd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DVD_DEVICE=$(/usr/bin/drutil status | /usr/bin/grep '/dev/disk' | cut -d ':' -f3)
+DVD_DEVICE=$(/usr/bin/drutil status | /usr/bin/grep '/dev/disk' | /usr/bin/cut -d ':' -f3)
 
 LONGEST_TITLE=$(/usr/local/bin/mplayer -dvd-device $DVD_DEVICE -nocache -identify dvdnav:// -frames 0 | /usr/bin/grep '_LENGTH=' | /usr/local/bin/gawk 'match($0, /TITLE_(.*)_LENGTH=(.*)$/, ary) && ary[2] > max_time { max_time = ary[2];  longest_title = ary[1] } END { print longest_title }')
 
-DISK_IDENTIFIER=$(/bin/echo $DVD_DEVICE | cut -d '/' -f3)
+DISK_IDENTIFIER=$(/bin/echo $DVD_DEVICE | /usr/bin/cut -d '/' -f3)
 
 MOVIE_NAME=$(/usr/sbin/diskutil list | /usr/bin/grep " $DISK_IDENTIFIER" | /usr/bin/tr -s ' ' | /usr/bin/cut -d ' ' -f3)
 
